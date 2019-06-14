dfe533b2f9d5cb163cc114982024bda14001ca93
Liu <LIUL5@legal.regn.net>
2019-06-14 14:37:39 +0800

    update code

--- run_parallel.py
+++ run_parallel.py
@@ -15,8 +15,8 @@ API_KEY = "12345"
 q = Queue(maxsize=0)
 
 browsers = [
-  {"os_api_name": "Win7x64-C2", "browser_api_name": "IE10", "name": "Python Parallel"},
-  {"os_api_name": "Win8.1", "browser_api_name": "Chrome43x64", "name": "Python Parallel"},
+    {"os_api_name": "Win7x64-C2", "browser_api_name": "IE10", "name": "Python Parallel"},
+    {"os_api_name": "Win8.1", "browser_api_name": "Chrome43x64", "name": "Python Parallel"},
 ]
 
 # put all of the browsers into the queue before pooling workers
@@ -31,7 +31,9 @@ def test_runner(q):
         try:
             browser = q.get()
             print("%s: Starting" % browser["browser_api_name"])
-            driver = webdriver.Remote(desired_capabilities=browser, command_executor="http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub" % (USERNAME, API_KEY) )
+            driver = webdriver.Remote(desired_capabilities=browser,
+                                      command_executor="http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub" % (
+                                      USERNAME, API_KEY))
             print("%s: Getting page" % browser["browser_api_name"])
             driver.get("http://crossbrowsertesting.com")
             print("%s: Quitting browser and ending test" % browser["browser_api_name"])
@@ -49,3 +51,47 @@ for i in range(num_threads):
     worker.start()
 
 q.join()
+
+##############################just note###################################################
+# import selenium
+# from selenium import webdriver
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# from time import sleep
+
+# driver = webdriver.Remote(command_executor="http://127.0.0.1:4444/wd/hub", desired_capabilities=DesiredCapabilities.CHROME)
+# driver = selenium.webdriver.remote.webdriver.WebDriver(command_executor="http://127.0.0.1:4444/wd/hub",
+#                                                        desired_capabilities=DesiredCapabilities.CHROME)
+# 1.实现串行多浏览器执行脚本
+# 浏览器数组
+# lists = ['chrome', 'firefox']
+
+# 通过不同的浏览器执行脚本
+# for browser in lists:
+#     driver = webdriver.Remote(
+#         command_executor='http://127.0.0.1:4444/wd/hub',
+#         desired_capabilities={'platform': 'ANY',
+#                               'browserName': browser,
+#                               'version': '',
+#                               'javascriptEnabled': True
+#                               })
+#     driver.get("http://www.baidu.com")
+#     driver.find_element_by_id("kw").send_keys("python")
+#     driver.find_element_by_id("su").click()
+#     sleep(2)
+# driver.quit()
+
+# # 2.实现串行多节点（分布式）执行脚本
+# d = {"http://192.168.40.220:5555/wd/hub": "chrome",
+#      "http://192.168.40.220:5556/wd/hub": "firefox"}
+# for host, browser in d.items():
+#     driver = webdriver.Remote(
+#         command_executor=host,
+#         desired_capabilities={'platform': 'ANY',
+#                               'browserName': browser,
+#                               'version': '',
+#                               'javascriptEnabled': True
+#                               })
+#     driver.get("http://www.baidu.com")
+#     driver.find_element_by_id("kw").send_keys("python")
+#     driver.find_element_by_id("su").click()
+#     sleep(2)
